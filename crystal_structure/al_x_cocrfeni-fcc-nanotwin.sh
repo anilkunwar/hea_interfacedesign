6eabc9ac7efc5f7298edfb4e185c0f9dffd33f81
Anil Kunwar <anilkunwar318@yahoo.com>
2025-09-06 20:19:13 +0200

    Update al_x_cocrfeni-fcc-nanotwin.sh

--- crystal_structure/al_x_cocrfeni-fcc-nanotwin.sh
+++ crystal_structure/al_x_cocrfeni-fcc-nanotwin.sh
@@ -9,10 +9,10 @@ atomsk ni_unit.xsf -duplicate 10 7 10 ni_super.xsf cfg
 atomsk ni_super.xsf -select random $m% Ni -substitute Ni Fe feni_super.xsf cfg
 atomsk feni_super.xsf -select random $m% Ni -substitute Ni Cr crfeni_super.xsf cfg
 atomsk crfeni_super.xsf -select random $m% Ni -substitute Ni Co cocrfeni_super.xsf cfg
-atomsk cocrfeni_super.xsf -select random $n% Ni -substitute Ni Al al0p5cocrfeni_super.xsf cfg
+atomsk cocrfeni_super.xsf -select random $n% Ni -substitute Ni Al al0p3cocrfeni_super.xsf cfg
 
 
 # Apply mirror symmetry
-atomsk al0p5cocrfeni_super.xsf -mirror 0 Y -wrap al0p5cocrfeni_mirror.xsf
+atomsk al0p3cocrfeni_super.xsf -mirror 0 Y -wrap al0p3cocrfeni_mirror.xsf
 # Merge the original super cell with the mirror supercell
-atomsk --merge Y 2 al0p5cocrfeni_super.xsf al0p5cocrfeni_mirror.xsf al0p5cocrfeni_nanotwin.xsf cfg
+atomsk --merge Y 2 al0p3cocrfeni_super.xsf al0p3cocrfeni_mirror.xsf al0p3cocrfeni_nanotwin.xsf cfg
